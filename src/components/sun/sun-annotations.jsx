ea5fad5c0db6678fab5684f4f8a5d5d897c90d90
MalloryWittwer <mallory.wittwer@gmail.com>
2022-03-10 18:05:54 +0100

    Fix 30th of the month

--- src/components/sun/sun-annotations.jsx
+++ src/components/sun/sun-annotations.jsx
@@ -2,61 +2,73 @@ import React, { Component } from "react";
 
 const coordinates = [
   {
+    id: 0,
     x: 14.700126,
     y: 80.67704,
     label: "Jan",
   },
   {
+    id: 1,
     x: 35.623596,
     y: 43.531193,
     label: "Feb",
   },
   {
+    id: 2,
     x: 72.382996,
     y: 21.927345,
     label: "Mar",
   },
   {
+    id: 3,
     x: 116.07411,
     y: 21.358879,
     label: "Apr",
   },
   {
+    id: 4,
     x: 152.25908,
     y: 42.862755,
     label: "May",
   },
   {
+    id: 5,
     x: 175.11176,
     y: 80.67704,
     label: "Jun",
   },
   {
+    id: 6,
     x: 176.22183,
     y: 123.78477,
     label: "Jul",
   },
   {
+    id: 7,
     x: 152.99017,
     y: 160.3457,
     label: "Aug",
   },
   {
+    id: 8,
     x: 115.56979,
     y: 181.90628,
     label: "Sep",
   },
   {
+    id: 9,
     x: 73.102165,
     y: 182.48071,
     label: "Oct",
   },
   {
+    id: 10,
     x: 35.034241,
     y: 160.9306,
     label: "Nov",
   },
   {
+    id: 11,
     x: 13.679568,
     y: 123.69225,
     label: "Dec",
@@ -71,7 +83,7 @@ class SunAnnotations extends Component {
           {coordinates.map((coords) => {
             return (
               <text
-                key={Number.parseInt(coords.label, 10) - 1}
+                key={coords.id}
                 transform="scale(.99998 1)"
                 x={coords.x}
                 y={coords.y}
